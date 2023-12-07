4224949a3370407bd97761e0ff8e72fc84cc99dc
rabhi alqadi <rabhialqadi22@gmail.com>
2023-12-07 11:23:19 -0800

    A cauliflower is a cabbage with a college education - Mark Twain

--- app.js
+++ app.js
@@ -24,3 +24,14 @@ $('.wand').css('color', 'red');
 const $pet = $('.pet').detach();
 // Append it somewhere else (replace 'newLocation' with the desired selector)
 $pet.appendTo('syns of lie');
+// Assuming your secret elements have the class 'secret', use jQuery to hide them
+$('.secret').hide('slow');
+// Chain the delay method to wait for 2000 milliseconds (2 seconds) before showing the elements
+$('.secret').hide('slow').delay(2000).show('slow');
+// Assuming your pet's leash has the class 'leash', use jQuery to add the class 'cabbage'
+$('.leash').addClass('cabbage');
+// Assuming your pet's leash has the classes 'leash' and 'cabbage', use jQuery to remove only the 'cabbage' class
+$('.leash').removeClass('cabbage');
+setTimeout(function () {
+  $('.pet.owl.leash').removeClass('cabbage');
+}, 2000);
