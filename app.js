226429e60776028a2755847e022078853198e4d7
rabhi alqadi <rabhialqadi22@gmail.com>
2023-12-07 11:03:32 -0800

    i added my first element and it was fun

---
+++ app.js
@@ -0,0 +1,13 @@
+$(() => {
+  const $container = $('#container');
+
+  console.log($container);
+
+  const $h1 = $('<h1>');
+
+  console.log($h1);
+
+  $h1.text('Hogwarts');
+
+  $container.append($h1);
+});
